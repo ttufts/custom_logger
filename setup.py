5e7e413c22cca7e058f39c59d5241a8cc0432f0c
Tom Tufts <thomas.t@digitaloperatives.com>
2017-03-07 08:47:20 -0500

    Fixed old data in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -27,23 +27,17 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    # url='https://github.com/pypa/sampleproject',
-    url='None',
+    url='https://github.com/ttufts/custom_logger',
 
     # Author details
     author='Thomas Tufts',
-    author_email='tom.tufts@hpe.com',
+    author_email='thomas.j.tufts@gmail.com',
 
     # Choose your license
-    # license='MIT',
-    # TODO Add license info
+    license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
@@ -51,22 +45,21 @@ setup(
         'Topic :: Software Development',
 
         # Pick your license as you wish (should match "license" above)
-        # 'License :: OSI Approved :: MIT License',
-        # TODO: Add license info
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
     ],
 
     # What does your project relate to?
-    keywords='tor_exit_monitor',
+    keywords='logging',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -82,29 +75,21 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-    },
+    extras_require={},
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-    },
+    package_data={},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[
-        # ('my_data', ['data/data_file'])
-    ],
+    data_files=[],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        # 'console_scripts': [
-        #     'sample=sample:main',
-        # ],
-    },
+    entry_points={},
 )
